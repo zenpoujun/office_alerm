f461f56a54aebe766bd2af38195fd0dfa58c5079
wdm_zen <wdm_zen@makotoos-iMac.local>
2017-05-26 17:24:05 +0900

    Initial Commit

---
+++ office_alerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  office_alerm
+//
+//  Created by wdm_zen on 2017/05/26.
+//  Copyright © 2017年 wdm_zen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ office_alermTests/office_alermTests.swift
@@ -0,0 +1,36 @@
+//
+//  office_alermTests.swift
+//  office_alermTests
+//
+//  Created by wdm_zen on 2017/05/26.
+//  Copyright © 2017年 wdm_zen. All rights reserved.
+//
+
+import XCTest
+@testable import office_alerm
+
+class office_alermTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
